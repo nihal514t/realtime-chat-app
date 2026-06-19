4e1060d45c7534a390e91636d047e014d60c2705
Muhammed Nihal <nihal514t@gmail.com>
2026-06-19 19:09:21 +0530

    Capitalisation issue fixed

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/sidebar";
 import MessageArea from "../components/MessageArea";
 import socket from "../services/socketService";
 
